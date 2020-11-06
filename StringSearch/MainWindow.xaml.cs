53118b23dd110879c9235301cf81a76d218e5b9d
Kevin Reiter <reiter.kk@gmail.com>
2020-11-06 10:00:52 +0100

    Generate an orderd List of words, shuffle it and display the result in a listbox.

--- StringSearch/MainWindow.xaml.cs
+++ StringSearch/MainWindow.xaml.cs
@@ -20,9 +20,23 @@ namespace StringSearch
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int dimension = 26;
+        string[] letters = new string[dimension] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        string[] wordList = new string[dimension * dimension * dimension * dimension];
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void BtnGenerateRandomWordList_Click(object sender, RoutedEventArgs e)
+        {
+            WordList.Create(wordList, letters, dimension);
+            WordList.Shuffle(wordList);
+            WordList.Display(wordList, LbRandomWordList);
+        }
+
+        private void LbRandomWordList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ StringSearch/WordList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace StringSearch
+{
+    class WordList
+    {
+        public static void Create(string[] wordList, string[] letters, int dimension)
+        {
+            int index = 0;
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    for (int k = 0; k < dimension; k++)
+                    {
+                        for (int l = 0; l < dimension; l++)
+                        {
+                            wordList[index++] = letters[i] + letters[j] + letters[k] + letters[l];
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Shuffle(string[] wordList)
+        {
+            //Fisher-Yates shuffle
+            Random rnd = new Random();
+            int n = wordList.Length;
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int randomIndex = rnd.Next(0, i + 1);
+                string tempString = wordList[i];
+                wordList[i] = wordList[randomIndex];
+                wordList[randomIndex] = tempString;
+
+            }
+        }
+
+        public static void Display(string[] wordList, ListBox listBox)
+        {
+            for (int i = 0; i < wordList.Length; i++)
+            {
+                listBox.Items.Add(wordList[i]);
+            }
+        }
+    }
+}
